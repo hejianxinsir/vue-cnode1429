b4e1047df768c4dc32de3b4ca29053441b104893
hejianxin <hejianxinsir@gmail.com>
2020-07-12 14:36:48 +0800

    完成 Header 组件样式

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+*{
+  margin: 0; padding: 0; list-style: none;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="header clearfix">
+    <img src="../assets/cnode.svg"/>
+    <input type="text" value="搜索" />
+    <ul>
+      <li><a href="#">首页</a></li>
+      <li><a href="#">新手入门</a></li>
+      <li><a href="#">API</a></li>
+      <li><a href="#">关于</a></li>
+      <li><a href="#">注册</a></li>
+      <li><a href="#">登录</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+.clearfix:after{
+  content: '';
+  display: block;
+  clear: both;
+}
+.header{
+  height: 50px;
+  background: #444;
+  font-size: 14px;
+
+  position: relative;
+}
+input{
+  border: 1px solid #888;
+  padding: 3px 7px;
+  width: 206px; height: 20px;
+  background: #888;
+  border-radius: 15px;
+  color: #ddd;
+
+  position: absolute;
+  top: 12px;
+}
+input:focus{
+  outline: none;
+  background: white;
+  transition: all 0.3s;
+  color: #444;
+}
+img{
+  max-width: 120px;
+  margin: 9px 10px 0 50px;
+}
+ul{
+  list-style: none;
+  float: right;
+  margin: 13px 0;
+  margin-right: 70px;
+}
+li{
+  display: inline-block;
+  color: #ccc;
+  padding: 0 15px;
+}
+a{
+  text-decoration: none;
+  color: #ccc;
+  font-weight: bold;
+}
+a:hover{
+  color: white;
+}
+
+</style>>
